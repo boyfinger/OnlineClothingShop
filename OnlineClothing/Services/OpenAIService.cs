d9b947a038e274615d3b9f8a639a86501d649c54
GwaarGura <tuananhtran180904@gmail.com>
2025-03-11 08:06:08 +0700

    add key to env file

--- OnlineClothing/Services/OpenAIService.cs
+++ OnlineClothing/Services/OpenAIService.cs
@@ -1,12 +1,13 @@
 ﻿using OpenAI.Chat;
-
+using DotNetEnv;
 namespace OnlineClothing.Services
 {
     public class OpenAIService : IOpenAIService
     {
         public ChatCompletion CheckDescription(string description)
         {
-            ChatClient client = new(model: "gpt-4o", apiKey: "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");
+            Env.Load();
+            ChatClient client = new(model: "gpt-4o", apiKey: Env.GetString("OPENAI_SECRET_KEY"));
 
             string prompt = $"""
         Check if the following product description meets these rules:
