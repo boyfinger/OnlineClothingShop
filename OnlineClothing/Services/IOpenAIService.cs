272d8e91f095ceb743b13175eaa5a02f509419f2
GwaarGura <tuananhtran180904@gmail.com>
2025-03-12 21:07:44 +0700

    integrate open ai

--- OnlineClothing/Services/IOpenAIService.cs
+++ OnlineClothing/Services/IOpenAIService.cs
@@ -1,9 +1,12 @@
-﻿using OpenAI.Chat;
+﻿using OpenAI.Assistants;
+using OpenAI.Chat;
 
 namespace OnlineClothing.Services
 {
     public interface IOpenAIService
     {
-        ChatCompletion CheckDescription(string description);
+        Task<string> CheckDescription(string description);
+
+        Task<string> CheckImage(string image, string description);
     }
 }
